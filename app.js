a6fc2271e41041626f9ae571d819c0beb6072b92
Phoenix Nicholson <phoenix.nicholson2008@gmail.com>
2021-10-04 15:42:16 -0700

    got parts of java script working like constants and if else statements

--- app.js
+++ app.js
@@ -1,8 +1,45 @@
 // import functions and grab DOM elements
+const randNum = (Math.floor(Math.random()*20)) + 1;
+const guessButton = document.getElementById('guess-btn');
+const userGuess = document.getElementById('user-guess').value;
+const reset = document.getElementById('reset');
+const resultP = document.getElementById('result');
 
 // initialize global state
-
+let numGuesses = 4;
+let result;
 // set event listeners 
+guessButton.addEventListener('click', ()=> {
+  numGuesses--;
+  // console.log(numGuesses);
+  
+  
+  if (userGuess === randNum) {
+    result = "You're correct!"
+  } 
+
+  else if (userGuess < randNum) {
+    result = 'Guess Higher!'
+  }
+
+  else if (userGuess > randNum) {
+    result = 'Guess Lower!'
+  }
+
+  if (userGuess >3){
+    document.getElementById('button').disabled = true;
+  }
+
+  
+
+
+  resultP.textContent = result;
+
+  console.log(userGuess);
+  
+
+});
+
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
