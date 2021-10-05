eccfee4f0e4fe946248c5ee6e6af216286ec1700
Phoenix Nicholson <phoenix.nicholson2008@gmail.com>
2021-10-05 10:51:16 -0700

    a
    git commit -m

--- cypress/integration/example.spec.js
+++ cypress/integration/example.spec.js
@@ -1,12 +1,20 @@
 
-// describe('Example Test', ()=>{
-//     before(()=>{
-//         cy.visit('/');
-//     });
+describe('Example Test', ()=>{
+    before(()=>{
+        cy.visit('/');
+    });
 
-//     describe('HTML elements', ()=>{   
-//         it('contains the alchemy logo', ()=>{
-//             // cy.get('.main-logo').should('be.visible');
-//         });
-//     });
-// });
+    describe('HTML elements', ()=>{   
+        it('should contain button', ()=>{
+            cy.get('#guess-btn').should('be.visible');
+        });
+    });
+    describe('user interaction', ()=> {
+        it('clicking on the button resets guesses', ()=> {
+            cy.get('#reset').click();
+            cy.get('#user-guess').should('be.visible');
+        });
+        // cy.get('#reset').click();
+        // cy.get('#result').should('be.visible');
+    });
+});
